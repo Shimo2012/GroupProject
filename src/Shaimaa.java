3720c35a5ee112cf16a1b7346be1de1972e4b8c7
shaym <shaymaa2012@gmail.com>
2023-12-17 11:58:28 -0500

    Welcome to our new project.

--- src/Shaimaa.java
+++ src/Shaimaa.java
@@ -1,6 +1,6 @@
 public class Shaimaa {
     public static void main(String[] args) {
-        System.out.println("Good morning!");
+        System.out.println("Good morning! Wish you a happy day!");
 
 
     }
