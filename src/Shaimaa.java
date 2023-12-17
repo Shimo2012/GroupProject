3ee36aac5e86221f6604c3891d9b5884b560136c
shaym <shaymaa2012@gmail.com>
2023-12-17 12:32:06 -0500

    Get ready!Start working!

--- src/Shaimaa.java
+++ src/Shaimaa.java
@@ -1,7 +1,8 @@
 public class Shaimaa {
     public static void main(String[] args) {
-        System.out.println("Good morning! Wish you a happy day! Let's start our work!");
+        System.out.println("Good morning! Wish you a happy day!");
         System.out.println("Now we are practicing GitHub");
+
 
 
     }
